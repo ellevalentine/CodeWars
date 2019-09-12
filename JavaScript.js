51b7c02ff3d4bb83bcb80e4600155fd828d59e79
ellevalentine <elle.valentine@hotmail.com>
2019-09-12 19:46:11 +0100

    update

--- JavaScript.js
+++ JavaScript.js
@@ -78,3 +78,96 @@ function GetSum(a,b) {
   }
   return sum;
 }
+
+// _____________________________________________________________________________________________________________________________________________________________
+
+// question:
+
+// In a small town the population is p0 = 1000 at the beginning of a year.
+// The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. 
+// How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+// Solution:
+
+
+function nbYear(p0, percent, aug, p) { 
+
+    per = (percent/100)
+  
+  for ( var n = 0; p0 < p; n++) {
+    
+     p0 = p0 + (p0*per) + aug }
+  
+      return n
+  }
+
+
+// _____________________________________________________________________________________________________________________________________________________________
+
+// question:
+
+// #Find the missing letter
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+
+
+// Solution:
+
+// using http://www.asciitable.com/
+
+function findMissingLetter(letters) {
+
+    str = letters.join("")
+      var i, j = 0, m = 122;
+      if (str) {
+          i = str.charCodeAt(0);
+          while (i <= m && j < str.length) {
+              if (String.fromCharCode(i) !== str.charAt(j)) {
+                  return String.fromCharCode(i);
+              }
+              i++; j++;
+          }
+      }
+      return undefined;
+  }
+
+// _____________________________________________________________________________________________________________________________________________________________
+
+// question:
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+// Leave punctuation marks untouched.
+
+// Solution:
+
+function pigIt(str){
+    var punctuation=[",","?","!",";",":",'"'];
+    var array=str.split(" ");
+    var last=array[array.length-1];
+    var newArray=array.slice(0,-1)
+                 .map(function(element){
+                    return element.slice(1) + element[0] + "ay";
+                 });
+                 
+    if(punctuation.indexOf(last) < 0){
+       return (newArray.concat(last.slice(1)+last[0]+"ay")).join(" ");
+    }else{
+       return (newArray.concat(last)).join(" ");
+    }
+  
+  }
+
+// _____________________________________________________________________________________________________________________________________________________________
+
+// question:
+
+
+// Solution:
